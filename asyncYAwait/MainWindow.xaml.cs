5642a26adee891dcd3a1df1911590d0f1ac51cc5
Yordy Nuñez <yordierik05@gmail.com>
2024-08-02 18:01:04 -0500

    Se crea repo para las expo que se hicieron con el instructor Corredor, se sube el de metodos y funciones con y sin parametros y async y await

---
+++ asyncYAwait/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ventana_Pizzeria
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txbLog.Text += "¡Tengo Hambre!" + Environment.NewLine;
+            Task<Pizza> pizzaTask = OrderPizza();
+            WatchTv();
+            Pizza pizza = await pizzaTask;
+            Eat(pizza);
+            txbLog.Text += "¡Satisfecho con mi productividad!" + Environment.NewLine;
+        }
+        async Task<Pizza> OrderPizza()
+        {
+            Pizza pizza = new Pizza();
+            txbLog.Text += "Pizza: Preparando..." + Environment.NewLine;
+            await Task.Delay(5000);
+            txbLog.Text += "Pizza: Enviando..." + Environment.NewLine;
+            await Task.Delay(5000);
+            txbLog.Text += "Pizza: Llegó..." + Environment.NewLine;
+            return pizza;
+        }
+
+        void WatchTv()
+        {
+            txbLog.Text += "Viendo TV..." + Environment.NewLine;
+        }
+
+        void Eat(Pizza pizza)
+        {
+            txbLog.Text += "Comiendo..." + Environment.NewLine;
+        }
+
+        private class Pizza
+        {
+
+        }
+
+    }
+}
+
+
---
+++ metodosFuncionesConSinParametros/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class MetodosYParametrosSinYConParametros
+{
+    //Declaramos el método sin parametros
+    public void mensajeCumpleaños()
+    {
+        Console.WriteLine("Hola, feliz cumpleaños :D");
+    }
+
+    //Declaramos la función de Resta con parametros
+    public int Resta(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        MetodosYParametrosSinYConParametros var = new MetodosYParametrosSinYConParametros();
+
+        //Imprimimos el mensaje
+        var.mensajeCumpleaños();
+
+        //Definimos una variable para guardar el resultado de nuestra resta
+        int resultado;
+
+        //Al resultado le asignamos la funcion de Resta y le damos dos numeros 
+        resultado = var.Resta(5, 2);
+
+        //Imprimimos el resultado
+        Console.WriteLine(resultado);
+    }
+}
