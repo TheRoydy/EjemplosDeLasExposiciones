127c5cc2392aa90debabdf36e9148a78c104d07d
Yordy Nuñez <yordierik05@gmail.com>
2024-08-05 17:46:44 -0500

    Se hicieron las clases y de polimorfismo, sobreescritura y sobrecarga

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class figurasGeometricas
+{
+    public virtual void lados()
+    {
+        Console.WriteLine("Lados de una figura geometrica");
+    }
+}
+
+public class Cuadrado : figurasGeometricas
+{
+    public override void lados()
+    {
+        Console.WriteLine("El cuadrado tiene 4 lados");
+    }
+}
+
+public class Triangulo : figurasGeometricas
+{
+    public override void lados()
+    {
+        Console.WriteLine("El triangulo tiene 3 lados");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        figurasGeometricas lasFigurasGeometricas = new figurasGeometricas();
+        figurasGeometricas elCuadrado = new Cuadrado();
+        figurasGeometricas elTriangulo = new Triangulo();
+
+        lasFigurasGeometricas.lados();
+        elCuadrado.lados();
+        elTriangulo.lados();
+    }
+}
+
